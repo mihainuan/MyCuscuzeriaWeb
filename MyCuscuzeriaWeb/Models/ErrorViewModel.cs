89930f45ced594dbedaecbc49bb3d4211822e388
Mihainuan De Sá Silva <mihainuan.silva@solutis.com.br>
2019-04-05 18:54:38 -0300

    Created ASP .NET Core Web Application

---
+++ MyCuscuzeriaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyCuscuzeriaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
